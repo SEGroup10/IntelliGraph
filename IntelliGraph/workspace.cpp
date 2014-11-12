c136d44d6af3d935d4caa6c785a92b377f49a592
Dc <dcduba@users.noreply.github.com>
2014-11-12 13:05:09 +0100

    Location fixes for workspace
    It was not possible to add elements at given coordinates. This fix
    should change that. Also some work towards the ability to add nodes to
    the workspace.

--- IntelliGraph/workspace.cpp
+++ IntelliGraph/workspace.cpp
@@ -6,11 +6,31 @@ Workspace::Workspace( QWidget *widget, QGraphicsView *elem )
     this->parent = widget;
 }
 
+Workspace::~Workspace()
+{
+    delete scene;
+}
+
 void Workspace::doStuff()
 {
     // We need a Scene on the GraphicsView to draw shapes
     scene = new QGraphicsScene( parent );
     drawingArea->setScene(scene);
+
+    //We can only add elements *inside* the bounding box of the scene, but by default this bounding box has
+    //no width or height, so we can't add anything...
+    //We set this bounding box to be a fixed value here so we can add something.
+    scene->setSceneRect(drawingArea->x(), drawingArea->y(), drawingArea->width()-10, drawingArea->height()-10);
+
+    //When using a dynamic scene rect, the scene rect is the bounding box of the elements.
+    //These invisible elements force the bounding box to be the size of the initial workspace
+    //allowing us to dynamically add elements in it.
+    scene->addRect(0,0,0,0,QPen(),QBrush());
+    scene->addRect(scene->width(),scene->height(),0,0,QPen(),QBrush());
+
+    //Scene rect back to automatic. Since we now have items in it, the bounding box will be at least the size
+    //of these elements. Mission accomplished.
+    scene->setSceneRect(QRectF());
 
     // As a test we draw a purple circle.
     QBrush purpleBrush(QColor(102, 0, 102));
@@ -19,7 +39,7 @@ void Workspace::doStuff()
     blackPen.setWidth(2);
 
     // Here we add the circle to the scene
-    ellipse = scene->addEllipse(10, 10, 100, 100, blackPen, purpleBrush);
+    QGraphicsEllipseItem* ellipse = scene->addEllipse(150, 150, 100, 100, blackPen, purpleBrush);
 
     // And make it movable
     ellipse->setFlag(QGraphicsItem::ItemIsMovable);
@@ -28,8 +48,10 @@ void Workspace::doStuff()
 
 void Workspace::handleClick( QMouseEvent *event ) {
     int x, y;
-    x = event->x() + drawingArea->horizontalScrollBar()->value() - drawingArea->x();
-    y = event->y() + drawingArea->verticalScrollBar()->value() - drawingArea->y();
+    x = event->x() - drawingArea->x() - drawingArea->horizontalScrollBar()->minimum() + drawingArea->horizontalScrollBar()->value();
+    y = event->y() - drawingArea->y() - drawingArea->verticalScrollBar()->minimum() + drawingArea->verticalScrollBar()->value();
     qDebug() << "Click at:" << x << "," << y << "relative to top-left of drawing area";
+
+    qDebug() << drawingArea->horizontalScrollBar()->minimum() << ":" << drawingArea->horizontalScrollBar()->maximum();
     //TODO fix this - values are off when scrollbar becomes visible for some reason
 }
--- IntelliGraph/workspace.h
+++ IntelliGraph/workspace.h
@@ -10,22 +10,23 @@
 #include <QDebug>
 #include <QScrollBar>
 
+#include "node.h"
+
 class Workspace
 {
 public:
     Workspace( QWidget *widget, QGraphicsView *elem );
+    ~Workspace();
     void doStuff();
     void handleClick( QMouseEvent *event );
 private:
     QWidget *parent;
     QGraphicsView *drawingArea;
-
-    QList <int>Nodes; // Hier moet <int> later aangepast worden aan de "Node" class! Int is alleen een placeholder.
-    QList <int>Edges; // Hier moet <int> later aangepast worden aan de "Edge" class!
-
-    //TODO find out if these are needed and if they need to be deleted when destroying this class
-    QGraphicsEllipseItem *ellipse;
     QGraphicsScene *scene;
+
+    QList<Node> nodes; // Hier moet <int> later aangepast worden aan de "Node" class! Int is alleen een placeholder.
+    QList<int> edges; // Hier moet <int> later aangepast worden aan de "Edge" class!
+
 };
 
 #endif // WORKSPACE_H
