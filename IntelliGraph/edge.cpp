f03f688f5030120a00e38aff34064d99914388b2
Tim de Heiden <tim.de.heiden@zonnet.nl>
2014-11-13 06:35:36 +0100

    minor changes to Edge class

---
+++ IntelliGraph/edge.cpp
@@ -0,0 +1,47 @@
+#include "edge.h"
+
+using namespace std;
+
+Edge::Edge(int newID, Node * newBegin, Node * newEnd): QGraphicsLineItem(newBegin->pos().x()+NODESIZE/2, newBegin->pos().y()+NODESIZE/2,
+                                                                         newEnd->pos().x()+NODESIZE/2, newEnd->pos().y()+NODESIZE/2, 0)
+{
+    ID = newID;
+    begin = newBegin;
+    end = newEnd;
+}
+
+Edge::~Edge()
+{
+
+}
+
+int Edge::getID()
+{
+    return ID;
+}
+
+string Edge::getName()
+{
+    return name;
+}
+
+void Edge::changeName(string newName)
+{
+    name = newName;
+    return;
+}
+
+void Edge::paint(QPainter *painter)
+{
+    QPen pen(Qt::black);
+    pen.setWidth(2);
+
+    painter->setPen(pen);
+    painter->drawLine(begin->pos().x(), begin->pos().y(), end->pos().x(), end->pos().y());
+}
+
+bool Edge::hasNode(Node *target)
+{
+    return (begin == target || end == target);
+}
+
